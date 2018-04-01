decd6ff2537f5a47c2d9137aaf276ec3fe94ccc7
root <sander@deboernet.eu>
2018-04-01 15:55:11 +0200

    documentation

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-	'propresenter_ip': 'localhost',
-	'propresenter_port': 50001,
-	'propresenter_password': 'password',
+	'propresenter_ip': 'localhost', 
+	'propresenter_port': 50001, // use the 'enable network' port, not the 'enable Stage Display App' port
+	'propresenter_password': 'password', // use the 'enable Stage Display App' password
         'filter_lines_starts_with': '['
 };
